46398dc2df5869c543b6359d49f559a00f90c1d2
CarrieSavage <carrie.savage@ucdconnect.ie>
2026-02-13 11:21:02 +0000

    ArrayQueue

--- src/project20280/stacksqueues/ArrayQueue.java
+++ src/project20280/stacksqueues/ArrayQueue.java
@@ -6,11 +6,13 @@ public class ArrayQueue<E> implements Queue<E> {
 
     private static final int CAPACITY = 1000;
     private E[] data;
-    private final int front = 0;
-    private final int size = 0;
+    private int front = 0;
+    private int size = 0;
 
     public ArrayQueue(int capacity) {
-        // TODO
+        data  = (E[]) new Object[capacity];
+        front = 0;
+        size = 0;
 
     }
 
@@ -31,7 +33,13 @@ public class ArrayQueue<E> implements Queue<E> {
 
     @Override
     public void enqueue(E e) {
-        // TODO
+        if (size == data.length) {
+            throw new IllegalStateException("Queue is full");
+        }
+
+        int avail = (front + size) % data.length;
+        data[avail] = e;
+        size++;
     }
 
     @Override
@@ -41,8 +49,15 @@ public class ArrayQueue<E> implements Queue<E> {
 
     @Override
     public E dequeue() {
-        // TODO
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+
+        E answer = data[front];
+        data[front] =  null;
+        front = (front + 1) % data.length;
+        size--;
+        return answer;
     }
 
     public String toString() {
